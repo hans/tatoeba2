01c500c4af1aa41b1e30b1b4f4a29eff1d5a2046
Hans Engel <engel@engel.uk.to>
2013-07-05 16:30:53 -0700

    Recording upload support

--- app/webroot/js/sentences.add_recording.js
+++ app/webroot/js/sentences.add_recording.js
@@ -19,116 +19,99 @@
 
 $(document).ready(function() {
 
-  $(".recordLink").click(function() {
-    var sentenceId = $(this).data("sentenceId");
-    var withAudio = $(this).data("withAudio");
+    $(".recordLink").click(function(){
+        var sentenceId = $(this).data("sentenceId");
 
-    var rootUrl = get_tatoeba_root_url();
+        var rootUrl = get_tatoeba_root_url();
+        var statusElement = $('#_' + sentenceId + '_recording_status');
 
-    // Initialize recorder
-    Recorder.initialize({
-      swfSrc: '/swf/recorder.swf'
-    });
+        // Initialize recorder
+        Recorder.initialize({
+            swfSrc: '/swf/recorder.swf'
+        });
 
-    /*
-     * Save translation.
-     */
-    function save() {
-      var sentenceText = $("#_" + sentenceId + "_text").val();
-      var selectLang = $("#translationLang_" + sentenceId).val();
-
-      if ($.trim(sentenceText) != "") {
-        unbind(); // very important
-        // This unbind() applies for the submit button and input field.
-        sentenceText = normalized_sentence(sentenceText);
-
-        $("#_" + sentenceId + "_translations").show();
-        $("#_" + sentenceId + "_loading").show();
-        $(".addTranslations").hide();
-
-        $.post(
-        rootUrl + "/sentences/save_translation", {
-          "id": sentenceId,
-          "selectLang": selectLang,
-          "value": sentenceText,
-          "parentOwnerName": parentOwnerName,
-          "withAudio": withAudio
-        }, function(data) {
-          $("#session_expired").remove();
-          $("#_" + sentenceId + "_loading").hide();
-          $("#_" + sentenceId + "_translations").prepend(data);
-          $("#_" + sentenceId + "_text").val('');
-        }, "html");
-
-      }
-    }
-
-    /*
-     * Function to unbind the handlers binded to the submit button, input field
-     * and cancel button. It is very important to unbind, otherwise a same
-     * translation will be save as many times as the user clicked on the
-     * "translate" icon.
-     */
-    function unbind() {
-      $("#_" + sentenceId + "_record").unbind('click');
-      $("#_" + sentenceId + "_stop_record").unbind('click');
-      $("#_" + sentenceId + "_play_recording").unbind('click');
-      $("#_" + sentenceId + "_submit_recording").unbind('click');
-      $("#_" + sentenceId + "_cancel_recording").unbind('click');
-    }
-
-    // Displaying translation input and hiding translations
-    $(".sentenceForm, #_" + sentenceId + "_translations").hide();
-    $("#recording_for_" + sentenceId).show();
-
-    var statusElement = $('#_' + sentenceId + '_recording_status');
-
-    $('#_' + sentenceId + '_record').click(function() {
-      statusElement.text('0:00');
-
-      Recorder.record({
-        start: function() {
-          statusElement.addClass('active');
-        },
-
-        progress: function(ms) {
-          var seconds = Math.floor(ms / 1000) % 60;
-          var minutes = Math.floor(ms / 1000 / 60);
-
-          var secondsPadded = seconds < 10 ? '0' + seconds : seconds;
-
-          statusElement.text(minutes + ':' + secondsPadded);
+        /*
+         * Submit recording.
+         */
+        function submitRecording(cb) {
+            Recorder.upload({
+                success: cb,
+                method: 'POST',
+                url: rootUrl + '/sentences/save_recording',
+                audioParam: 'recording',
+                params: {
+                    sentenceId: sentenceId
+                }
+            });
         }
-      });
+
+        function stopRecording() {
+            Recorder.stop();
+            statusElement.removeClass('active');
+        }
+
+        /*
+         * Function to unbind the handlers binded to the submit button, input field
+         * and cancel button. It is very important to unbind, otherwise a same
+         * translation will be save as many times as the user clicked on the
+         * "translate" icon.
+         */
+        function unbind(){
+            $("#_" + sentenceId + "_record").unbind('click');
+            $("#_" + sentenceId + "_stop_record").unbind('click');
+            $("#_" + sentenceId + "_play_recording").unbind('click');
+            $("#_" + sentenceId + "_submit_recording").unbind('click');
+            $("#_" + sentenceId + "_cancel_recording").unbind('click');
+        }
+
+        // Displaying translation input and hiding translations
+        $(".sentenceForm, #_" + sentenceId + "_translations").hide();
+        $("#recording_for_" + sentenceId).show();
+
+
+
+        $('#_' + sentenceId + '_record').click(function() {
+            statusElement.text('0:00');
+
+            Recorder.record({
+                start: function () {
+                    statusElement.addClass('active');
+                },
+
+                progress: function (ms) {
+                    var seconds = Math.floor(ms / 1000) % 60;
+                    var minutes = Math.floor(ms / 1000 / 60);
+
+                    var secondsPadded = seconds < 10 ? '0' + seconds : seconds;
+
+                    statusElement.text(minutes + ':' + secondsPadded);
+                }
+            });
+        });
+
+        $('#_' + sentenceId + '_stop_record').click(stopRecording);
+
+        $('#_' + sentenceId + '_play_recording').click(function() {
+            Recorder.play();
+        });
+
+        // Submit recording
+        $("#_" + sentenceId + "_submit_recording").click(function(){
+            stopRecording();
+
+            submitRecording(function() {
+                statusElement.text('Your recording has been uploaded!');
+            });
+        });
+
+        // Cancel
+        $("#_" + sentenceId + "_cancel_recording").click(function(){
+            stopRecording();
+
+            unbind(); // very important
+            $("#_" + sentenceId + "_translations").show();
+            $(".addRecording").hide();
+        });
     });
-
-    $('#_' + sentenceId + '_stop_record').click(function() {
-      statusElement.removeClass('active');
-
-      Recorder.stop();
-    });
-
-    $('#_' + sentenceId + '_play_recording').click(function() {
-      Recorder.play();
-    });
-
-    // Submit recording
-    $("#_" + sentenceId + "_submit_recording").click(function() {
-      statusElement.removeClass('active');
-      Recorder.stop();
-
-      // TODO
-    });
-
-    // Cancel
-    $("#_" + sentenceId + "_cancel_recording").click(function() {
-      statusElement.removeClass('active');
-      Recorder.stop();
-
-      unbind(); // very important
-      $("#_" + sentenceId + "_translations").show();
-      $(".addRecording").hide();
-    });
-  });
 
 });
